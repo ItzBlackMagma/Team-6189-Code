71865fff251a9a6f652dafe56e04d51387764257
GameChanger77 <gamer4christ2004@gmail.com>
2021-03-08 09:26:08 -0600

    Fixed the capitalization of the r in the run method.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/Kinematics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/Kinematics.java
@@ -11,6 +11,7 @@ public class Kinematics extends Thread {
     private double xPos = 0, yPos = 0, rotation = 0;
     private double motor1, motor2, motor3, motor4, xVelocity, yVelocity, rVelocity, resultantVelocity, xDis, yDis, theta;
     private double GLOBAL_X = 0, GLOBAL_Y = 0, PREV_X = 0, PREV_Y = 0;
+    private int lastM1 = 0, lastM2 = 0, lastM3 = 0, lastM4 = 0;
 
     public boolean isRunning = true;
 
@@ -18,7 +19,7 @@ public class Kinematics extends Thread {
         this.robot = robot;
     }
 
-    public void Run(){
+    public void run(){
         while (isRunning) {
             updateRobotVelocity();
             updatePosition();
@@ -40,14 +41,17 @@ public class Kinematics extends Thread {
         GLOBAL_Y = (xDis * Math.cos(90 - theta)) + (yDis * Math.sin(90 - theta));
 
         pose.update(GLOBAL_X - PREV_X, GLOBAL_Y - PREV_Y, robot.getRotation());
+
+        PREV_X = GLOBAL_X;
+        PREV_Y = GLOBAL_Y;
     }
 
     private void updateRobotVelocity(){
         // gets wheel velocities
-        motor1 = getMotorSpeed(robot.motor1.getCurrentPosition());
-        motor2 = getMotorSpeed(robot.motor2.getCurrentPosition());
-        motor3 = getMotorSpeed(robot.motor3.getCurrentPosition());
-        motor4 = getMotorSpeed(robot.motor4.getCurrentPosition());
+        motor1 = getMotorSpeed(robot.motor1.getCurrentPosition() - lastM1);
+        motor2 = getMotorSpeed(robot.motor2.getCurrentPosition() - lastM2);
+        motor3 = getMotorSpeed(robot.motor3.getCurrentPosition() - lastM3);
+        motor4 = getMotorSpeed(robot.motor4.getCurrentPosition() - lastM4);
 
         // calculates the robot's velocities in x, y and r relative to the robot
         xVelocity = (motor1 + motor2 + motor3 + motor4) * (R/4); // longitudinal component
@@ -55,10 +59,16 @@ public class Kinematics extends Thread {
         rVelocity = (-motor1 + motor2 - motor3 + motor4) * (R/4); // angular component
 
         resultantVelocity = xVelocity + yVelocity + rVelocity; // the final velocity of the robot will be the sum of each component
+
+        // reset the last motor positions
+        lastM1 = robot.motor1.getCurrentPosition();
+        lastM2 = robot.motor2.getCurrentPosition();
+        lastM3 = robot.motor3.getCurrentPosition();
+        lastM4 = robot.motor4.getCurrentPosition();
     }
 
     private double getMotorSpeed(int pos){ // returns meters per second
-        return (double) ((pos * (1 / CPI)) / (sleepTime * 1000));
+        return (pos * (1 / CPI)) / (sleepTime * 1000);
     }
 
 }
